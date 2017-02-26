016de603868578bda24db94687e4fdd416609356
Sam Garg <samgarg86@gmail.com>
2017-02-26 22:58:22 +0000

    Heroku fixes

--- server.prod.js
+++ server.prod.js
@@ -1,25 +1,21 @@
 var express = require('express');
 var path =  require('path');
 var open = require('open');
-// import compression from 'compression';
-// import favicon from 'serve-favicon';
+import compression from 'compression';
+import favicon from 'serve-favicon';
 
 /*eslint-disable no-console */
 
 var port = process.env.PORT || 3000;
 var app = express();
 
-// app.use(compression());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'dist')));
-// app.use(favicon(path.join(__dirname,'assets','public','favicon.ico')));
+app.use(favicon(path.join(__dirname,'favicon.ico')));
 
 app.all('*', function(request, response) {
     response.sendFile(__dirname + '/dist/index.html');
 });
-
-// app.get('/:newsId', function (req, res) {
-//     res.send(req.params);
-// });
 
 app.listen(port, function(err) {
     if (err) {
